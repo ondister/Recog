7e5e56308968bd25a067891542b455c6e304fd0c
ondister <ondister@gmail.com>
2015-07-08 11:47:43 +0300

    первый релиз

---
+++ testblank/ApplicationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Recog
+{
+    public class ApplicationInfo
+    {
+        public static string GetDirectory()
+        {
+            return Path.GetDirectoryName(Application.ExecutablePath);
+        }
+        
+    }
+}
